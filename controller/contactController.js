8a204dcff03e2f5be814021a1875e648293029d5
Ranveer098 <95100082+Ranveer098@users.noreply.github.com>
2023-11-02 00:07:27 +0530

    init

--- controller/contactController.js
+++ controller/contactController.js
@@ -1,18 +1,20 @@
 //we will giviing label to nparticular API
 
+const asyncHandler = require("express-async-handler"); //to handle the error in async await we use this package
+
 //@desc Get all contact
 //@route GET /api/contact
 //@access Public
 
-const getContacts = async(req, res) => {       //to resolve the promise we use async and await in mongoose
+const getContacts = asyncHandler(async(req, res) => {       //to resolve the promise we use async and await in mongoose to catch error we use try and catch
   res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 //@desc post new  contacts
 //@route GET/api/contacts
 //@access Public
 
-const createContact =async (req, res) => {
+const createContact =asyncHandler(async (req, res) => {
   //get request
 
   console.log("This request Body",req.body); //req.body is used to get the data from the body of th
@@ -23,34 +25,34 @@ const createContact =async (req, res) => {
      throw new Error("Please enter all fields" );
   } 
   res.status(201).json({ message: "create contact" });
-};
+});
 
 //@desc GET  contacts
 //@route GET/api/contacts:id
 //@access Public
 
-const getContact =async (req, res) => {
+const getContact =asyncHandler(async (req, res) => {
   //get request
   res.status(200).json({ message: `Get contact for ${req.params.id}` }); //req.params.id is used to get the id from the url
-};
+});
 
 //@desc Update  contacts
 //@route PUT /api/contacts:id
 //@access Public
 
-const UpdateContact =async (req, res) => {
+const UpdateContact =asyncHandler(async (req, res) => {
   //get request
   res.status(200).json({ message: `update contact for ${req.params.id}` }); //req.params.id is used to get the id from the url
-};
+});
 
 //@desc delete contacts
 //@route DELETE /api/contacts:id
 //@access Public
 
-const deleteContact =async (req, res) => {
+const deleteContact =asyncHandler(async (req, res) => { //asyncHandler is used to handle the error in async await
   //get request
   res.status(200).json({ message: `Delete contact for  ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
